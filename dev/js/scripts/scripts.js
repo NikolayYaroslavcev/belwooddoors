a8906bb5cb556c09901f8d7ebd7b0e2f9e729f43
Nikolay <sweeterthanfurby@mail. git config --global user.email sweeterthanfurby@mail>
2023-03-23 17:54:39 +0300

    fill: styled

--- dev/js/scripts/scripts.js
+++ dev/js/scripts/scripts.js
@@ -1,124 +1,3 @@
-// window.addEventListener('load', () => {
-//   if (
-//     /MSIE 9/i.test(navigator.userAgent) ||
-//     /rv:11.0/i.test(navigator.userAgent) ||
-//     /rv:10.0/i.test(navigator.userAgent)
-//   ) {
-//     document.body.className = 'ie';
-//     window.location = '/ie.html';
-//   }
-//
-//   setGlobalCssVar();
-//
-//   lazyLoadInstance = new LazyLoad({
-//     elements_selector: '.lazy',
-//   });
-// });
-//
-// let resizeTimer;
-// window.addEventListener('resize', () => {
-//   clearTimeout(resizeTimer);
-//
-//   resizeTimer = setTimeout(() => {
-//     resizeFunction([setGlobalCssVar]);
-//   }, 250);
-// });
-//
-// document.addEventListener('click', (e) => {
-//   const eTarget = e.target.closest(`[${DATA_ACTION}]`);
-//   let actionList = [];
-//
-//   if (eTarget) {
-//     const actiones = eTarget.getAttribute(DATA_ACTION);
-//     actionList = actiones.split(' ');
-//
-//     actionList.map((item) => {
-//       if (item === BURGER_MENU_SWITCHER) {
-//         const burgerMenu = document.querySelector('[data-burger-menu]');
-//
-//         eTarget.classList.toggle(ACTIVE_CLASS);
-//
-//         if (burgerMenu) {
-//           scrollToggle();
-//           menuBgOverlayToggle();
-//           burgerMenu.classList.toggle(ACTIVE_CLASS);
-//         }
-//       }
-//
-//       if (item === 'accordion-switcher') {
-//         const DROPDOWN_SELECTOR = '[data-accordion-dropdown]';
-//         const acc = eTarget.closest('[data-accordion]');
-//         const dropDown = acc.querySelector(DROPDOWN_SELECTOR);
-//
-//         acc.classList.toggle(ACTIVE_CLASS);
-//         dropDown.slideToggle();
-//
-//         getSiblings(acc).map((item) => {
-//           item.classList.remove(ACTIVE_CLASS);
-//
-//           const itemDropDown = item.querySelector(DROPDOWN_SELECTOR);
-//
-//           if (itemDropDown) {
-//             itemDropDown.slideUp();
-//           }
-//         });
-//       }
-//
-//       if (item === 'load-youtube-iframe') {
-//         onYouTubeIframeAPIReady(eTarget, eTarget.getAttribute('data-id'));
-//       }
-//
-//       if (item === 'tabs-button') {
-//         const TABS_BTN_SELECTOR = 'data-tab-btn';
-//         const TABS_CONTENT_SELECTOR = 'data-tab-content';
-//         const dataTabSelector = eTarget.getAttribute(TABS_BTN_SELECTOR);
-//
-//         eTarget.classList.add(ACTIVE_CLASS);
-//
-//         getSiblings(eTarget.closest('li')).forEach((item) => {
-//           item.querySelector(`[${TABS_BTN_SELECTOR}]`).classList.remove(ACTIVE_CLASS);
-//         });
-//
-//         const targetContent = document.querySelectorAll(
-//           `[${TABS_CONTENT_SELECTOR}="${dataTabSelector}"]`
-//         );
-//
-//         if (targetContent.length) {
-//           targetContent.forEach((item) => {
-//             getSiblings(item).forEach((itemInner) => {
-//               itemInner.classList.remove(ACTIVE_CLASS);
-//             });
-//
-//             item.classList.add(ACTIVE_CLASS);
-//           });
-//         }
-//       }
-//     });
-//   }
-// });
-//
-// document.addEventListener('change', (e) => {
-//   const eTarget = e.target.closest(`[${DATA_ACTION}]`);
-//   let actionList = [];
-//
-//   if (eTarget) {
-//     const actiones = eTarget.getAttribute(DATA_ACTION);
-//     actionList = actiones.split(' ');
-//
-//     actionList.map((item) => {
-//       // if (item === SIDEBAR_ACTION_STRING) {
-//       // }
-//     });
-//   }
-// });
-//
-// const setGlobalCssVar = () => {
-//   setCssVariable('--viewport-width', `${window.innerWidth}px`);
-//   setCssVariable('--viewport-height', `${window.innerHeight}px`);
-//   setCssVariable('--header-height', getNodeCssValue(document.querySelector('.header'), 'height'));
-// };
-
-
 const titles = document.querySelectorAll('.accordion-title');
 const contents = document.querySelectorAll('.accordion-content');
 
@@ -142,3 +21,25 @@ titles.forEach(item => item.addEventListener('click', () => {
         activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
     }
 }))
+
+// slider
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    rewind: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 2
+        },
+        769: {
+            slidesPerView: 3
+        },
+        1600: {
+            slidesPerView: 6
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
